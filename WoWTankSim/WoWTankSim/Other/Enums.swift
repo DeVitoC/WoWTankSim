eb177f63b2d3d5e504ac7f87fc13ad6d2a73ca3b
Christopher DeVito <christopher.devito@protonmail.com>
2021-01-25 15:12:56 -0500

    Added Enums file with enums. Initially added enums for User Spec and gear Source.

---
+++ WoWTankSim/WoWTankSim/Other/Enums.swift
@@ -0,0 +1,21 @@
+//
+//  Enums.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 1/25/21.
+//
+
+import Foundation
+
+enum Spec {
+    case Blood, Vengeance, Guardian, Brewmaster
+    case ProtectionPaladin, ProtectionWarrior
+}
+
+enum Source {
+    case RaidLFR, RaidNormal, RaidHeroic, RaidMythic
+    case WorldBosses
+    case DungeonNormal, DungeonHeroic, DungeonMythic
+    case DungeonMythicPlus(Int)
+    case PVPHonor, PVPConquest, Crafted, BOE
+}
