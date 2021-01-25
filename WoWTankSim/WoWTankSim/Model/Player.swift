d3867839573613f6ab027235277c67b91281d766
Christopher DeVito <christopher.devito@protonmail.com>
2021-01-25 15:57:19 -0500

    Added Player struct with spec, source, talents, and soul bind options.

---
+++ WoWTankSim/WoWTankSim/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 1/25/21.
+//
+
+import Foundation
+
+struct Player {
+    var spec: Spec
+    var source: [Source]
+    var talents: TalentTree
+    var soulbind: Soulbind
+
+}
