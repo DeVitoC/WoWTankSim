c263d5b241d9faa895b2b05df48d713ca7da5d66
Christopher DeVito <christopher.devito@protonmail.com>
2021-01-26 17:06:03 -0500

    Finished setting ups UI elements in SelectClassViewController and updated methods to create elements in UIElements file. Added player property to manage player status as user moves through the various choices.

--- WoWTankSim/WoWTankSim/Other/UIElements.swift
+++ WoWTankSim/WoWTankSim/Other/UIElements.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 func createButton(text: String, bgColor: UIColor = .purple) -> UIButton {
-    let button = UIButton()
+    let button = UIButton(frame: CGRect(x: 0, y: 0, width: 100, height: 30))
+    button.translatesAutoresizingMaskIntoConstraints = false
     button.setTitle(text, for: .normal)
     button.backgroundColor = bgColor
     button.tintColor = .white
@@ -16,23 +17,34 @@ func createButton(text: String, bgColor: UIColor = .purple) -> UIButton {
     return button
 }
 
-func createHeader(text1: String, text2: String?) -> UIView {
-    let headerView = UIView()
+func createHeader(xPos: Int, text1: String, text2: String?) -> UIView {
+    let headerView = UIView(frame: CGRect(x: xPos, y: 80, width: 250, height: 70))
     let label1 = UILabel()
     let label2 = UILabel()
     let stackView = UIStackView()
 
-    headerView.frame = CGRect(x: 0, y: 0, width: 500, height: 200)
+//    headerView.translatesAutoresizingMaskIntoConstraints = false
+    label1.translatesAutoresizingMaskIntoConstraints = false
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+
     headerView.backgroundColor = .systemIndigo
     label1.tintColor = .black
     label1.text = text1
-    
+    stackView.axis = .vertical
+    stackView.alignment = .center
+    stackView.addArrangedSubview(label1)
+
     headerView.addSubview(stackView)
+    NSLayoutConstraint.activate([
+        stackView.centerYAnchor.constraint(equalTo: headerView.centerYAnchor),
+        stackView.centerXAnchor.constraint(equalTo: headerView.centerXAnchor)
+    ])
 
     if text2 != nil {
+        label2.translatesAutoresizingMaskIntoConstraints = false
         label2.tintColor = .black
         label2.text = text2
-
+        stackView.addArrangedSubview(label2)
     }
 
 
--- WoWTankSim/WoWTankSim/Views/SelectClassViewController.swift
+++ WoWTankSim/WoWTankSim/Views/SelectClassViewController.swift
@@ -8,29 +8,30 @@
 import UIKit
 
 class SelectClassViewController: UIViewController {
-
-    // MARK: - Properties
-    let header = createHeader(text1: "Welcome!", text2: "Click a Class Below to Sim")
-    let paladinButton = createButton(text: "PALADIN")
-    let nextButton = createButton(text: "NEXT")
+    let player = Player()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let header = createHeader(xPos: Int(view.frame.width/2 - 125), text1: "Welcome!", text2: "Click a Class Below to Sim")
+        let paladinButton = createButton(text: "PALADIN")
+
         view.addSubview(header)
         view.addSubview(paladinButton)
-        view.addSubview(nextButton)
 
         NSLayoutConstraint.activate([
             header.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
             header.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             paladinButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             paladinButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20),
-            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
 
-        // Do any additional setup after loading the view.
+        paladinButton.addTarget(self, action: #selector(clickSpec), for: .touchUpInside)
+
+    }
+
+    @objc func clickSpec(_ sender: Any) {
+        performSegue(withIdentifier: "SelectSourceSegue", sender: self)
     }
     
 
