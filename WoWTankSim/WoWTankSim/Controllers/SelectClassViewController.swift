f3bd47fa3fac27c59f51ed28e9acd02c324c3d17
Christopher DeVito <christopher.devito@protonmail.com>
2021-01-26 15:48:29 -0500

    Added header, Paladin button, and Next button to SelectClassViewController with initial constraints.

--- WoWTankSim/WoWTankSim/Controllers/SelectClassViewController.swift
+++ WoWTankSim/WoWTankSim/Controllers/SelectClassViewController.swift
@@ -9,8 +9,26 @@ import UIKit
 
 class SelectClassViewController: UIViewController {
 
+    // MARK: - Properties
+    let header = createHeader(text1: "Welcome!", text2: "Click a Class Below to Sim")
+    let paladinButton = createButton(text: "PALADIN")
+    let nextButton = createButton(text: "NEXT")
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        view.addSubview(header)
+        view.addSubview(paladinButton)
+        view.addSubview(nextButton)
+
+        NSLayoutConstraint.activate([
+            header.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            header.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            paladinButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            paladinButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            nextButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20),
+            nextButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
 
         // Do any additional setup after loading the view.
     }
