aa9dc15c04a9005cd3662a20712cf74b14eedf39
Christopher DeVito <christopher.devito@protonmail.com>
2021-01-28 11:23:49 -0500

    Moved UI elements to separate method and added buttons for all tanks and import your own character.

--- WoWTankSim/WoWTankSim/Views/SelectClassViewController.swift
+++ WoWTankSim/WoWTankSim/Views/SelectClassViewController.swift
@@ -9,26 +9,36 @@ import UIKit
 
 class SelectClassViewController: UIViewController {
 
+    // MARK: - Properties
     let playerController = PlayerController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUIElements()
+    }
 
+    func setupUIElements() {
         let header = createHeader(xPos: Int(view.frame.width/2 - 125), text1: "Welcome!", text2: "Click a Class Below to Sim")
         let paladinButton = createButton(text: "PALADIN")
+        let warriorButton = createButton(text: "WARRIOR")
+        let dkButton = createButton(text: "DEATH KNIGHT")
+        let dhButton = createButton(text: "DEMON HUNTER")
+        let druidButton = createButton(text: "DRUID")
+        let monkButton = createButton(text: "MONK")
+        let importButton = createButton(text: "IMPORT YOUR CHARACTER")
+        let buttonStackView = UIStackView(arrangedSubviews: [dkButton, dhButton, druidButton, monkButton, paladinButton, warriorButton, importButton])
 
         view.addSubview(header)
-        view.addSubview(paladinButton)
+        view.addSubview(buttonStackView)
 
         NSLayoutConstraint.activate([
             header.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
             header.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            paladinButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            paladinButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            buttonStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
         ])
 
         paladinButton.addTarget(self, action: #selector(clickSpec), for: .touchUpInside)
-
     }
 
     @objc func clickSpec(_ sender: Any) {
