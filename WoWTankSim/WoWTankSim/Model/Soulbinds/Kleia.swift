aa2b1168ab3978e6a7c512250c66959442328cd3
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-03 17:50:45 -0500

    Added struct for Kleia's soul bind tree.

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Kleia.swift
@@ -0,0 +1,19 @@
+//
+//  Kleia.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/3/21.
+//
+
+import Foundation
+
+struct Kleia {
+    let row1 = ["Valiant Strikes"]
+    var row2 = ["Potency", "Endurance"]
+    var row3 = ["Mentorship", "Ascendant Phial"]
+    var row4 = ["Finess"]
+    var row5 = ["Cleansing Rites", "Ever Forward"]
+    var row6 = ["Endurance"]
+    var row7 = ["Potency", "Endurance", "Finess"]
+    var row8 = ["Bearer's Pursuit", "Pointed Courage", "Resonant Accolades"]
+}
