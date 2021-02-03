618a5d64516575fd7fe827d882610233d7152e65
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-03 18:11:54 -0500

    Added struct for Korayn's Soulbind Tree

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Korayn.swift
@@ -0,0 +1,19 @@
+//
+//  Korayn.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/3/21.
+//
+
+import Foundation
+
+struct Korayn {
+    let row1 = ["Wild Hunt Tactics"]
+    var row2 = ["Potency", "Endurance"]
+    var row3 = ["Horn of the Wild Hunt", "Wild Hunt's Charge"]
+    var row4 = ["Finess"]
+    var row5 = ["Vorkai Sharpening Techniques", "Get In Formation"]
+    var row6 = ["Endurance"]
+    var row7 = ["Potency", "Endurance", "Finess"]
+    var row8 = ["Face Your Foes", "First Strike", "Hold the Line"]
+}
