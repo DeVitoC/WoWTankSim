cb73e83972c8c9b27cc36ae927ddffead1839f05
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-03 18:01:08 -0500

    Added struct for Niya's Soulbind tree

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Niya.swift
@@ -0,0 +1,19 @@
+//
+//  Niya.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/3/21.
+//
+
+import Foundation
+
+struct Niya {
+    let row1 = ["Grove Invigoration"]
+    var row2 = ["Potency", "Endurance"]
+    var row3 = ["Run Without Tiring", "Stay on the Move"]
+    var row4 = ["Finess"]
+    var row5 = ["Nature's Splendor", "Swift Patrol"]
+    var row6 = ["Endurance"]
+    var row7 = ["Potency", "Endurance", "Finess"]
+    var row8 = ["Niya's Tools: Burrs", "Niya's Tools: Poison", "Niya's Tools: Herbs"]
+}
