e7ebfb47aa32a3aeb66889831abfe87a599a2635
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-03 18:07:59 -0500

    Added struct for Dreamweaver Soulbind tree

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Dreamweaver.swift
@@ -0,0 +1,19 @@
+//
+//  Dreamweaver.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/3/21.
+//
+
+import Foundation
+
+struct Dreamweaver {
+    let row1 = ["Podtender"]
+    var row2 = ["Endurance", "Finess"]
+    var row3 = ["Potency"]
+    var row4 = ["Soothing Voice", "Social Butterfly", "Empowered Chrysalis"]
+    var row5 = ["Potency", "Endurance", "Finess"]
+    var row6 = ["Endurance"]
+    var row7 = ["Faerie Dust", "Somnambulist"]
+    var row8 = ["Field of Blossoms"]
+}
