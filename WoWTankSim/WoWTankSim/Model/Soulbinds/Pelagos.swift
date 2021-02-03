a9f8590ff6112a09a9c8f662de3156964622470e
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-03 16:09:54 -0500

    Added text for all conduit slots for all rows of the Pelagos Soulbind

--- WoWTankSim/WoWTankSim/Model/Soulbinds/Pelagos.swift
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Pelagos.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 struct Pelagos {
-    let row1 = "Combat Meditation"
-    var row2 = "Conduit"
+    let row1 = ["Combat Meditation"]
+    var row2 = ["Potency", "Endurance", "Finess"]
+    var row3 = ["Focusing Mantra", "Road of Trials", "Phial of Patience"]
+    var row4 = ["Endurance"]
+    var row5 = ["Bond of Friendship", "Cleansed Vestments"]
+    var row6 = ["Potency"]
+    var row7 = ["Endurance", "Finess"]
+    var row8 = ["Let Go of the Past"]
 }
