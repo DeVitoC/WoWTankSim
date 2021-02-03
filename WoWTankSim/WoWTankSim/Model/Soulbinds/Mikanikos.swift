7860dce0469df45e214b50a92f3feadc1c2f339e
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-03 17:54:56 -0500

    Added Struct for Mikanikos' Soulbind tree

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Mikanikos.swift
@@ -0,0 +1,19 @@
+//
+//  Mikanikos.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/3/21.
+//
+
+import Foundation
+
+struct Kleia {
+    let row1 = ["Bron's Call to Action"]
+    var row2 = ["Potency", "Endurance"]
+    var row3 = ["Forgelite Filter", "Charged Additive"]
+    var row4 = ["Finess"]
+    var row5 = ["Regenerative Materials", "Resilient Plumage"]
+    var row6 = ["Endurance"]
+    var row7 = ["Potency", "Endurance", "Finess"]
+    var row8 = ["Soulsteel Clamps", "Hammer of Genesis", "Sparkling Driftglobe Core"]
+}
