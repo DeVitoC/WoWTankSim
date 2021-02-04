6904e0bc4ae6d6383055e2c4f6e7a49be3e69d68
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-04 01:46:47 -0500

    Added struct for Theortar's Soulbind tree

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Theotar.swift
@@ -0,0 +1,19 @@
+//
+//  Theotar.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/4/21.
+//
+
+import Foundation
+
+struct Theotar: SoulbindProtocol {
+    var row1 = ["Soothing Shade"]
+    var row2 = ["Potency", "Endurance"]
+    var row3 = ["Watch the Shoes!", "Leisurely Gait"]
+    var row4 = ["Finess"]
+    var row5 = ["Life of the Party", "Exquisite Ingredients"]
+    var row6 = ["Endurance"]
+    var row7 = ["Potency", "Endurance", "Finess"]
+    var row8 = ["Token of Appreciation", "Refined Palate", "Wasteland Propriety"]
+}
