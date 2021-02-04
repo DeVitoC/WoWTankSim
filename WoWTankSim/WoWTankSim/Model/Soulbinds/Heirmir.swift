5afcf16aa02297f46a5283e7dbe8197a316d43e6
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-04 01:15:34 -0500

    Added struct for Heirmir's soul bind tree

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Heirmir.swift
@@ -0,0 +1,19 @@
+//
+//  Heirmir.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/4/21.
+//
+
+import Foundation
+
+struct Heirmir: SoulbindProtocol {
+    var row1 = ["Forgeborne Reveries"]
+    var row2 = ["Potency", "Endurance"]
+    var row3 = ["Resourceful Fleshcrafting", "Serrated Spaulders"]
+    var row4 = ["Finess"]
+    var row5 = ["Runeforged Spurs", "Bonesmith's Satchel"]
+    var row6 = ["Endurance"]
+    var row7 = ["Potency", "Endurance", "Finess"]
+    var row8 = ["Heirmir's Arsenal: Gorestompers", "Heirmir's Arsenal: Marrowed Gemstone", "Heirmir's Arsenal: Ravenous Pendant"]
+}
