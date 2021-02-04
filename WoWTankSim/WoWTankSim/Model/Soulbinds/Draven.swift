269487ba9fa88e06c0dd9723a0f48a7a980cdd09
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-04 01:45:39 -0500

    Added Struct for Draven's soul bind tree

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Draven.swift
@@ -0,0 +1,19 @@
+//
+//  Draven.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/4/21.
+//
+
+import Foundation
+
+struct Draven: SoulbindProtocol {
+    var row1 = ["Service In Stone"]
+    var row2 = ["Potency", "Endurance"]
+    var row3 = ["Move As One", "Enduring Gloom"]
+    var row4 = ["Finess"]
+    var row5 = ["Unbreakable Body", "Expedition Leader"]
+    var row6 = ["Endurance"]
+    var row7 = ["Potency", "Endurance", "Finess"]
+    var row8 = ["Hold Your Ground", "Superior Tactics", "Built for War"]
+}
