f8d82a635610045022f3d61b6e1a56a9117d0550
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-04 01:03:48 -0500

    Added struct for Marileth's Soulbind

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Marileth.swift
@@ -0,0 +1,19 @@
+//
+//  Marileth.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/4/21.
+//
+
+import Foundation
+
+struct Marileth: SoulbindProtocol {
+    var row1 = ["Volatile Solvent"]
+    var row2 = ["Potency", "Endurance", "Finess"]
+    var row3 = ["Ooz's Frictionless Coating", "Travel With Bloop", "Plaguey's Preemptive Strike"]
+    var row4 = ["Endurance"]
+    var row5 = ["Kevin's Keyring", "Plagueborn Cleansing Slime"]
+    var row6 = ["Potency"]
+    var row7 = ["Endurance", "Finess"]
+    var row8 = ["Ultimate Form"]
+}
