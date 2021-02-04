0fad586208ae81ab3f10318d4c61c5b79d6e9b96
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-04 01:45:10 -0500

    Added struct for Nadjia's Soulbind tree

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Nadjia.swift
@@ -0,0 +1,19 @@
+//
+//  Nadjia.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/4/21.
+//
+
+import Foundation
+
+struct Nadjia: SoulbindProtocol {
+    var row1 = ["Thrill Seeker"]
+    var row2 = ["Potency", "Endurance"]
+    var row3 = ["Agent of Chaos", "Fancy Footwork"]
+    var row4 = ["Finess"]
+    var row5 = ["Friends in Low Places"]
+    var row6 = ["Endurance"]
+    var row7 = ["Potency", "Endurance", "Finess"]
+    var row8 = ["Familiar Predicaments", "Exacting Preparation", "Dauntless Duelist"]
+}
