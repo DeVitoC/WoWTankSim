8c1094e5cece1fa9012f7a38576324d9379026c4
Christopher DeVito <christopher.devito@protonmail.com>
2021-02-04 01:09:18 -0500

    Added struct for Emeni's Soulbind tree

---
+++ WoWTankSim/WoWTankSim/Model/Soulbinds/Emeni.swift
@@ -0,0 +1,19 @@
+//
+//  Emeni.swift
+//  WoWTankSim
+//
+//  Created by Christopher Devito on 2/4/21.
+//
+
+import Foundation
+
+struct Emeni: SoulbindProtocol {
+    var row1 = ["Lead By Example"]
+    var row2 = ["Potency", "Endurance"]
+    var row3 = ["Emeni's Magnificent Skin", "Emeni's Ambulatory Flesh"]
+    var row4 = ["Finess"]
+    var row5 = ["Cartilaginous Legs", "Hearth Kidneystone"]
+    var row6 = ["Endurance"]
+    var row7 = ["Potency", "Endurance", "Finess"]
+    var row8 = ["Gristled Toes", "Gnashing Chompers", "Sulfuric Emission"]
+}
